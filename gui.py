3c38306c77e8660d41540377bb48ac2461deb6fa
Claire Zhong (EXT-Nokia) <claire.zhong.ext@nokia.com>
2025-07-11 16:04:31 -0400

    GUI

---
+++ gui.py
@@ -0,0 +1,33 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QVBoxLayout, QWidget
+
+class MyWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle("My First PyQt5 App")
+        self.setGeometry(100, 100, 300, 200)
+
+        # Create a label and button
+        self.label = QLabel("Hello, PyQt5!", self)
+        self.button = QPushButton("Click Me", self)
+        self.button.clicked.connect(self.on_button_click)
+
+        # Layout to arrange widgets
+        layout = QVBoxLayout()
+        layout.addWidget(self.label)
+        layout.addWidget(self.button)
+        self.setLayout(layout)
+
+    def on_button_click(self):
+        self.label.setText("Button clicked!")
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MyWindow()
+    window.show()
+    sys.exit(app.exec_())
+
+
