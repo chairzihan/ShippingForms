b32a222ae17325942897aac7d2aea2ddb4ba92ab
Claire Zhong (EXT-Nokia) <claire.zhong.ext@nokia.com>
2025-07-18 10:28:44 -0400

    changed requirements.txt and added yaml

--- saveYaml.py
+++ saveYaml.py
@@ -1,2 +1,33 @@
 import yaml
+import os
+from pathlib import Path
+import yaml
+from pyaml import dump
+
+def get_config_path():
+    # Get the user home directory
+    home = Path.home()
+
+    
+    config_dir = home / '.yourapp'
+    config_dir.mkdir(exist_ok=True)  # create if doesn't exist
+
+    # YAML config file path
+    config_file = config_dir / 'config.yaml'
+
+    return config_file
+
+def load_config():
+    config_file = get_config_path()
+    if config_file.exists():
+        with open(config_file, 'r') as f:
+            return yaml.safe_load(f)
+    else:
+        # Return default config if file doesn't exist
+        return {}
+
+def save_config(data):
+    config_file = get_config_path()
+    with open(config_file, 'w') as f:
+        f.write(dump(data))
 
