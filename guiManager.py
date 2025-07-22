9f34032166706afeebc352c73d26d7a350f3fbce
Claire Zhong (EXT-Nokia) <claire.zhong.ext@nokia.com>
2025-07-22 10:08:20 -0400

    setTodayDate works

--- guiManager.py
+++ guiManager.py
@@ -2,8 +2,10 @@ from PyQt5.QtWidgets import QApplication, QDialog
 from mainGUI import Ui_mainFrame
 from senderGUI import Ui_SenderInformation
 from receiverGUI import Ui_senderFrame
+import datetime
+from PyQt5.QtCore import QDate
 
-
+progress = 0
 
 class SenderDialog(QDialog):
     def __init__(self):
@@ -24,15 +26,27 @@ class MainDialog(QDialog):
         self.ui.setupUi(self)
         self.ui.newSenderButton.clicked.connect(self.open_sender_dialog)
         self.ui.newReceiverButton.clicked.connect(self.openReceiverDialog)
+        self.ui.setTodayDateButton.clicked.connect(self.setTodayDate)
 
     #open sender screen
     def open_sender_dialog(self):
         dialog = SenderDialog()
         dialog.exec_() 
-    
+
+    #open receiver screen
     def openReceiverDialog(self):
         receiverFrame = ReceiverDialog()
         receiverFrame.exec_()
+
+    #set Today's date
+    def setTodayDate(self):
+        today = QDate.currentDate()
+        self.ui.dateEdit.setDate(today)
+        progress=25
+        self.ui.progressBar.setValue(progress)
+
+
+        
 
 
 if __name__ == "__main__":
